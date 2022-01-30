1a75d70255c43311c06b1405a72f071c04c8e0e6
The Coding Goggles <the.coding.goggles@gmail.com>
2022-01-30 14:22:44 +0530

    Update nightlight.py

--- nightlight.py
+++ nightlight.py
@@ -7,6 +7,6 @@ def process(img):
     added_image = cv2.addWeighted(img,alpha_beta,img,alpha_beta,0)
     added_image = cv2.GaussianBlur(added_image,(5,5),cv2.BORDER_DEFAULT)
     added_image = cv2.detailEnhance(added_image, sigma_s=1, sigma_r=10000)
-    added_image = cv2.fastNlMeansDenoisingColored(added_image,None,10,10,5,85)
+    added_image = cv2.fastNlMeansDenoisingColored(added_image,None,10,10,5,100)
 
     return added_image
